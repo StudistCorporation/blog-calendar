27d5961bda6291c984caed44fcdbfb4401eea538
ERDOS Balint <valerauko@gmail.com>
2023-05-18 15:02:00 +0900

    Add basic logging

--- project.clj
+++ project.clj
@@ -2,13 +2,15 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [ch.qos.logback/logback-classic "1.4.4"]
                  [mount "0.1.16"]
                  [aleph "0.6.1"]
                  [metosin/reitit "0.5.18"]]
   :main ^:skip-aot calendar.core
   :target-path "target/%s"
+  :resource-paths ["config" "public"]
   :profiles {:dev {:source-paths ["dev/api"]
-                   :resource-paths ["public"]
                    :dependencies [[org.clojure/tools.namespace "1.3.0"]]}
              :uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/calendar/core.clj
+++ src/calendar/core.clj
@@ -1,17 +1,21 @@
 (ns calendar.core
   (:require [aleph.http :as http]
+            [clojure.tools.logging :as log]
             [mount.core :refer [defstate start stop]]
             [calendar.router :as router])
   (:import [java.io
             Closeable])
   (:gen-class))
 
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
 (defstate http-server
   :start
-  (http/start-server
-   router/handler
-   {:port 3000
-    :compression true})
+  (let [options {:port 3000
+                 :compression true}]
+    (log/debug (str "API server running at :" (:port options)))
+    (http/start-server router/handler options))
   :stop
   (.close ^Closeable http-server))
 
