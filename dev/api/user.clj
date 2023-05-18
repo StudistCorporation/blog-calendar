d736c063e9ae57c8f235a34c9d852736c7122af1
ERDOS Balint <valerauko@gmail.com>
2023-05-18 21:16:47 +0900

    Add convenience fns

--- dev/api/user.clj
+++ dev/api/user.clj
@@ -1,7 +1,13 @@
 (ns user
   (:require [clojure.repl :refer [doc source]]
             [clojure.tools.namespace.repl :refer [refresh set-refresh-dirs]]
-            [mount.core :refer [defstate start stop]]))
+            [camel-snake-kebab.core :refer [->kebab-case-string]]
+            [mount.core :refer [defstate start stop]])
+  (:import [java.time
+            ZonedDateTime
+            ZoneId]
+           [java.time.format
+            DateTimeFormatter]))
 
 (set-refresh-dirs "dev/api" "src")
 
@@ -9,3 +15,22 @@
   []
   (stop)
   (refresh :after 'mount.core/start))
+
+(defn now
+  []
+  (let [utc (ZoneId/of "UTC")
+        formatter (DateTimeFormatter/ofPattern "uuuuMMddHHmmss")
+        timestamp (ZonedDateTime/now utc)]
+    (.format formatter timestamp)))
+
+(defn create-migration
+  [name]
+  (let [base (str (now) "-" (->kebab-case-string name))
+        folder "resources/migrations/"
+        up-name (str folder base ".up.sql")
+        down-name (str folder base ".down.sql")
+        command "chown 1000:1000 "]
+    (spit up-name (str "-- " up-name "\n"))
+    (spit down-name (str "-- " down-name "\n"))
+    (.exec (Runtime/getRuntime) (str command up-name))
+    (.exec (Runtime/getRuntime) (str command down-name))))
