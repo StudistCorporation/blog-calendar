8b4db063eb2b5660f0590cae4baa1305eb3db063
ERDOS Balint <valerauko@gmail.com>
2023-05-18 15:58:52 +0900

    Fix static resource handling

--- src/calendar/router.clj
+++ src/calendar/router.clj
@@ -6,5 +6,5 @@
    (ring/router
     [["/api" (constantly {:status 200 :body "Hello, world"})]])
    (ring/routes
-    (ring/create-resource-handler {:path "/"})
+    (ring/create-resource-handler {:path "" :root ""})
     (ring/create-default-handler))))
