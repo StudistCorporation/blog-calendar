d9abb98ed958a0a2d76ea7b45921d8868fa5a48f
ERDOS Balint <valerauko@gmail.com>
2023-05-19 00:03:10 +0900

    Adjust meta

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject calendar "0.1.0"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :license {:name "AGPL-3.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.2.4"]
