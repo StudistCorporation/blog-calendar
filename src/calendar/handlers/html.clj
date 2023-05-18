a237c4e35277afbe35925501956bc8d44cf80a52
ERDOS Balint <valerauko@gmail.com>
2023-05-18 15:48:01 +0900

    Add handler for index.html

---
+++ src/calendar/handlers/html.clj
@@ -0,0 +1,10 @@
+(ns calendar.handlers.html
+  (:require [clojure.java.io :as io]))
+
+(def file-body
+  (slurp (io/resource "index.html")))
+
+(defn handler
+  [_]
+  {:status 200
+   :body file-body})
