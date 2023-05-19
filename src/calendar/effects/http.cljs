65ad69a9ce40a3629483c5079d575767743538a8
ERDOS Balint <valerauko@gmail.com>
2023-05-19 20:20:47 +0900

    Convenience

--- src/calendar/effects/http.cljs
+++ src/calendar/effects/http.cljs
@@ -15,7 +15,9 @@
        (.then (fn [response]
                 (if (<= 200 (.-status response) 299)
                   (-> (.json response)
-                      (.then #(rf/dispatch (conj on-success %)))
+                      (.then #(rf/dispatch
+                               (conj on-success
+                                     (js->clj % :keywordize-keys true))))
                       (.catch #(rf/dispatch (conj on-error %))))
                   (rf/dispatch (conj on-error response)))))
        (.catch #(rf/dispatch (conj on-error %))))))
