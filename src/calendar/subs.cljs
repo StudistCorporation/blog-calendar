1d7172fdb9ae6271a4ec15011a7150a1f2a0fa18
ERDOS Balint <valerauko@gmail.com>
2023-05-19 21:31:10 +0900

    Add detailed subs for calendar

--- src/calendar/subs.cljs
+++ src/calendar/subs.cljs
@@ -10,6 +10,19 @@
  :-> :calendar)
 
 (rf/reg-sub
+ ::filled-days
+ :<- [::days]
+ :-> count)
+
+(rf/reg-sub
+ ::total-days
+ :<- [::calendar]
+ (fn [{:keys [start end]}]
+   (let [start-day (.getDate (js/Date. start))
+         end-day (.getDate (js/Date. end))]
+     (- end-day start-day))))
+
+(rf/reg-sub
  ::day
  (fn [db [_ day]]
    (get-in db [:days day])))
